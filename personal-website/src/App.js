5d6389ea774bb7d913ee6b2e9b1ed20813030e13
letmegro <nick.kor23@gmail.com>
2023-05-22 00:00:57 -0400

    created website

---
+++ personal-website/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ParticleOptions from './Config/ParticleConfig';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ personal-website/src/Footer.js
@@ -0,0 +1,13 @@
+import './footer.css';
+
+function Footer(){
+    return (
+        <div className='footer'>
+            <p className="designedBy">
+                Site Designed By: Nicolas Korsunski
+            </p>
+        </div>
+    );
+}
+
+export default Footer;
