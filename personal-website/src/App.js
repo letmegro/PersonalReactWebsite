bff2c506740b202f7f1cf5674c7d4de54f5955ac
letmegro <nick.kor23@gmail.com>
2023-05-22 23:10:10 -0400

    updated front page

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -6,8 +6,23 @@ function App() {
     <div className="App">
       <Particle/>
       <header className="App-header">
-        
+      
       </header>
+      <div className='info-div'>
+        <h1 id='welcome-h1'>
+          Welcome
+        </h1>
+        <h2 className='sentence-sec'>
+         <br/> My name is Nicolas Korsunski
+        </h2>
+        <h2 className='sentence-sec'>
+        <br/> And this is my online resume
+        </h2>
+        <h2 className='sentence-sec'>
+        <br/> And Portfolio.
+        </h2>
+        
+      </div>
     </div>
   );
 }
