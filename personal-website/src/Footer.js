b6c114610a889f173af88c03f165a93cff09635d
Nicolas Korsunski <84050410+letmegro@users.noreply.github.com>
2023-06-02 23:49:35 -0400

    Update Footer.js

--- personal-website/src/Footer.js
+++ personal-website/src/Footer.js
@@ -1,11 +1,21 @@
 import './footer.css';
-
+// add hyperlinks to detailedd pages in the footer.
 function Footer(){
     return (
         <div className='footer'>
             <p className="designedBy">
                 Site Designed By: Nicolas Korsunski
             </p>
+            <a href="sf">
+                <p className='contact'>
+                    Contact
+                </p>
+            </a>
+            <a href="gd">
+                <p>
+                    About me
+                </p>
+            </a>
         </div>
     );
 }
